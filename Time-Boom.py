fd5ffe1711567e078a324899064ba3c3d8580479
Vedran <vedde13@gmail.com>
2025-05-20 15:05:01 +0200

    Update Time-Boom.py

--- Time-Boom.py
+++ Time-Boom.py
@@ -1,75 +1,77 @@
 import random
 import time
-print ("-Time-Boom-")
+print("-Time Bombers-")
 count = 0
 
 random_value = random.randint(1, 2)
 
+
 while True:
+    
+    if count == 0:
+            print("◻◻◻◻◻◻◻◻◻◻")
+    if count == 1:
+            print("◼◻◻◻◻◻◻◻◻◻")
+    if count == 2:
+            print("◼◼◻◻◻◻◻◻◻◻")
+    if count == 3:
+            print("◼◼◼◻◻◻◻◻◻◻")
+    if count == 4:
+            print("◼◼◼◼◻◻◻◻◻◻")
+    if count == 5:
+            print("◼◼◼◼◼◻◻◻◻◻")
+    if count == 6:
+            print("◼◼◼◼◼◼◻◻◻◻")
+    if count == 7:
+            print("◼◼◼◼◼◼◼◻◻◻")
+    if count == 8:
+            print("◼◼◼◼◼◼◼◼◻◻")
+    if count == 9:
+            print("◼◼◼◼◼◼◼◼◼◻")
+    if count == 10:
+            print("◼◼◼◼◼◼◼◼◼◼")
+    
+    
+    
+    choice = input("- ")
+    if choice == "1":
+        print("You played 1")
+        count += 1
+        if count >= 10:
+                print("Boom!")
+                print("You lose!")
+                print(f"The count ended at {count}")
+                break
+            
+        else: 
+                time.sleep(1)
+                count += random_value
+                print(f"Computer played {random_value}")
+                time.sleep(0.3)
+                print(f"The count is {count}")
+    elif choice == "2":
+        print("You played 2")
+        count += 2
+        
+        if count >= 10:
+                print("Boom!")
+                print("You lose!")
+                print(f"The count ended at {count}")
+                break
+        
+        else: 
+            time.sleep(1)
+            count += random_value
+            print(f"Computer played {random_value}")
+            time.sleep(0.3)
+            print(f"The count is {count}")
+    elif choice == "count":
+        print(f"The count is {count}")
+    else:
+        print("Invalid input")
 
-  if count == 0:
-    print("◻◻◻◻◻◻◻◻◻◻")
-  if count == 1:
-    print("◻◼◻◻◻◻◻◻◻◻")
-  if count == 2:
-    print("◼◼◻◻◻◻◻◻◻◻")
-  if count == 3:
-    print("◼◼◼◻◻◻◻◻◻◻")
-  if count == 4:
-    print("◼◼◼◼◻◻◻◻◻◻")
-  if count == 5:
-    print("◼◼◼◼◼◻◻◻◻◻")
-  if count == 6:
-    print("◼◼◼◼◼◼◻◻◻◻")
-  if count == 7:
-    print("◼◼◼◼◼◼◼◻◻◻")
-  if count == 8:
-    print("◼◼◼◼◼◼◼◼◻◻")
-  if count == 9:
-    print("◼◼◼◼◼◼◼◼◼◻")
-  if count == 10:
-    print("◼◼◼◼◼◼◼◼◼◼")
-
-choice = input("- ")
-if choice == "1":
-  print("You played 1")
-  count +=1
-  if count >= 10:
-    print("Boom!")
-    print("You lose!")
-    print(f"The count ended at {count}")
-    break
-  else:
-    time.sleep(1)
-    count += random_value
-    print(f"Computer played {random_value}")
-    time.sleep(0.3)
-    print(f"The count is {count}")
-elif choice == "2":
-  print("You played 2")
-  count +=2
-
-  if count >= 10:
-    print("Boom!")
-    print("You lose!")
-    print(f"The count ended at {count}")
-  else:
-    time.sleep(1)
-    count += random_value
-    print(f"Computer played {random_value}")
-    time.sleep(0.3)
-    print(f"The count is {count}")
-else: 
-  print("Invalid input")
-if count >= 10:
-  print("Boom!")
-  print("You win!")
-  print(f"The count ended at {count}")
-  break
-
-
-
-  
-
-
-
+    if count >= 10:
+        print("Boom!")
+        print("You win!")
+        print(f"The count ended at {count}")
+        break
